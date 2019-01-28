a48e2880aa3150e7310f64653ed26873e2a8fd41
DanUk2 <43887736+DanUk2@users.noreply.github.com>
2019-01-28 04:53:04 +0000

    Version 4

--- CassiniMission.py
+++ CassiniMission.py
@@ -1,5 +1,5 @@
 # The Cassini mission
-# Version 3
+# Version 4
 # Daniel Hitchcock
 # 25/01/2019
 import time
@@ -127,6 +127,7 @@ def chooseHelp():
         return helpLeo
 
 def rover1():
+    hoursLeft = 5
     print()
     time.sleep(1)
     print("*40 minutes into exhibition*")
@@ -210,8 +211,75 @@ def rover2():
     print(name,", how do we get out this?")
     time.sleep(2)
     print()
+    chooseQuicksand()
+    print()
+    time.sleep(2)
+    print("Apollo: If everyone is ready lets continue")
+    time.sleep(2)
+    print("Hours Left [", hoursLeft, "]")
+    timeCheck()
 
-
+def rover3():
+    print()
+    time.sleep(2)
+    print("*Leo Shivers*")
+    time.sleep(1)
+    print("Leo: When did it get so co-cold?")
+    print()
+    time.sleep(2)
+    print("Pandora: Titan has a few glaciers left that create their own climate")
+    time.sleep(2)
+    print(" Imagine global warming on steroids")
+    time.sleep(2)
+    print()
+    print("Apollo: Frozen lake up ahead!")
+    print()
+    time.sleep(2)
+    print("Leo: Brilliant, we'll never make it in time")
+    time.sleep(2)
+    print()
+    print("Pandora: Wait,", name, "do you still have your GPS?")
+    time.sleep(2)
+    print(" We can check how long it will take us to reach the signal")
+    print()
+    time.sleep(2)
+    print(inv)
+    print()
+    time.sleep(1)
+    if "gps" in inv:
+        print("[TIME TO CROSS: 1 HOUR]")
+        time.sleep(1)
+        print("[TIME TO GO AROUND: 2 HOURS]")
+        time.sleep(4)
+    else:
+        print("Leo: Damn it, the storm scrambled mine too")
+    print()
+    time.sleep(2)
+    print("Pandora: I think we should go around")
+    time.sleep(2)
+    print(" Looking at how thin the ice is, i'd say we have a 33% chance of making it")
+    print()
+    time.sleep(3)
+    print("Apollo: We have to take the chance")
+    time.sleep(2)
+    print(" We cant afford to waste any more time")
+    time.sleep(2)
+    print(" If we stay out here by nightfall we'll freeze either way")
+    time.sleep(2)
+    print()
+    print("Leo: Are you crazy!? We will die! We're going around!")
+    time.sleep(2)
+    print()
+    print("Pandora: The odds are in our favour, but i'd rather not test my luck Sir")
+    print()
+    time.sleep(2)
+    print("Apollo: We'll leave it up to " + name)
+    time.sleep(2)
+    print(" What should we do?")
+    print()
+    choice2 = choosePath2()
+    checkPath2(choice2)
+    timeCheck()
 
 
 
@@ -265,7 +333,7 @@ def checkPath(chosenPath):
         print("Apollo: BRACE FOR IMPACT!")
         time.sleep(3)
         print()
-        randomItem = random.randint(1, 2)
+        randomItem = random.randint(1, 2, 3)
         if randomItem == 1:
             inv.remove("laser cutter")
             print("*Laser Cutter removed from inventory*")
@@ -288,6 +356,216 @@ def checkPath(chosenPath):
         print()
 
 
+def chooseQuicksand():
+    quickSand = ""
+    global hoursLeft
+    while quickSand != "rev" and quickSand != "pull":
+        quickSand = input("Choose a Way out [Rev] or [Pull]").lower()
+    if quickSand == "rev":
+        hoursLeft -= 1
+        print()
+        print("Apollo: Are you sure?")
+        time.sleep(2)
+        print(" I hope you know what you're doing")
+        print()
+        time.sleep(2)
+        print("Leo: Hit the gas", name, "!")
+        print()
+        time.sleep(2)
+        print("*Engine starts revving*")
+        print()
+        time.sleep(2)
+        print("Leo: Come on!")
+        time.sleep(1)
+        print(" I know you can do it baby!")
+        time.sleep(2)
+        print()
+        print("Apollo: I think its working!")
+        print()
+        time.sleep(2)
+        print("*The Rover bursts out of the sand*")
+        print()
+        time.sleep(2)
+        print("Leo: HAHA! I told you it would work!")
+        print()
+        time.sleep(2)
+        print("*Engine breaks down*")
+        time.sleep(2)
+        print()
+        print("Pandora: You were saying?")
+        print()
+        time.sleep(2)
+        print("Apollo:..")
+        time.sleep(2)
+        print(" Leo how long will this take to fix?")
+        time.sleep(2)
+        print()
+        print("Leo: Depends, with a Laser Cutter it could be a few minutes")
+        time.sleep(2)
+        print(" With a welder I could fix it within the hour")
+        time.sleep(2)
+        print(name + " what have you got?")
+        print()
+        time.sleep(1)
+        chooseEngine()
+        print()
+        time.sleep(2)
+        print(inv)
+
+
+    else:
+        hoursLeft -= 2
+        print()
+        print("Apollo: Are you sure about this?")
+        time.sleep(2)
+        print(" This will take too long")
+        print()
+        time.sleep(2)
+        print("Pandora: It is the safest option!")
+        time.sleep(2)
+        print(" Don't listen to them", name, ", this was the smart choice")
+        time.sleep(2)
+        print()
+        print("Apollo: Okay everyone start pulling on the count of 3")
+        time.sleep(2)
+        print(" 1")
+        time.sleep(1)
+        print(" 2")
+        time.sleep(1)
+        print(" 3")
+        time.sleep(1)
+        print(" PULL!")
+        print()
+        time.sleep(1)
+        print("*Rover slowly emerges out of sand*")
+        print()
+        time.sleep(2)
+        print("Apollo: Almost there! PULL!")
+        time.sleep(2)
+        print()
+        print("*Rover edges onto dry land to safety*")
+        print()
+        time.sleep(2)
+        print("Pandora: Good work guys, I told you this would work")
+        print()
+        time.sleep(2)
+        print("Leo: At what cost? We only have", hoursLeft, "hours left")
+        print()
+        time.sleep(2)
+        print("Apollo: Then stop talking and lets get a move on")
+        time.sleep(2)
+        print()
+        print("Leo: Sorry Sir! Yes Sir!")
+
+def chooseEngine():
+    engine = ""
+    global hoursLeft
+    while engine != "laser cutter" and engine != "welding tool":
+        engine = input("Choose a Tool [Laser Cutter] or [Welding Tool]: ").lower()
+    if engine == "laser cutter":
+        if engine in inv:
+            print("Leo: Thank god, that's going to save a lot of time")
+            print()
+            time.sleep(2)
+            inv.remove("laser cutter")
+            print("*Laser Cutter removed from Inventory*")
+            return engine
+        else:
+            print("You don't have that tool")
+            chooseEngine()
+    elif engine == "welding tool":
+        hoursLeft -= 1
+        print("Leo: Okay that will do, give me an hour")
+        print()
+        time.sleep(2)
+        inv.remove("welding tool")
+        print("*Welding Tool removed from Inventory*")
+        return engine
+
+def choosePath2():
+    path2 = ""
+    while path2 != "cross" and path2 != "go around":
+        path2 = input("Choose a Path [Cross] or [Go Around]: ").lower()
+    return path2
+
+def checkPath2(chosenPath2):
+    global hoursLeft
+    if chosenPath2 == "cross":
+        print()
+        print("Apollo: It's settled, start driving " + name)
+        print()
+        time.sleep(2)
+        print("*Ice starts cracking behind*")
+        time.sleep(2)
+        print()
+        print("Pandora: SIR! THE ICE IS STARTING TO CRACK!")
+        print()
+        time.sleep(2)
+        print("Leo: FLOOR IT", name.upper(), "!")
+        print()
+        time.sleep(1)
+        print("Pandora: ITS CATCHING UP TO US!")
+        print()
+        time.sleep(1)
+        print("Apollo: JUST A LITTLE FURTHER AND WE CAN MAKE IT")
+        print()
+        time.sleep(2)
+        chance = (1,2,3)
+        randomIce = random.choice(chance)
+        if randomIce == 1:
+            print("Pandora: KEEP GO-")
+            print()
+            time.sleep(1)
+            print("*You fell through the ice*")
+            print()
+            playAgain = ""
+            while playAgain != "yes" and playAgain != "y":
+                playAgain = input("Do you want to play again? [Yes] or [Y] to continue playing: ").lower()
+            if playAgain == "yes" or "y":
+                print()
+                print("*Starting from checkpoint*")
+                choosePath2()
+            else:
+                return
+        else:
+            hoursLeft -= 1
+            print("Pandora: KEEP GOING!")
+            print()
+            time.sleep(2)
+            print("*Rover drifts onto land*")
+            print()
+            time.sleep(2)
+            print("Pandora: Phew, that was a close one")
+            time.sleep(2)
+            print(" Good driving " + name)
+    elif chosenPath2 == "go around":
+        hoursLeft -= 2
+        print()
+        print("Apollo: Fine, start driving")
+        time.sleep(2)
+        print(" We better hope we make it in time")
+
+
+def timeCheck():
+    if hoursLeft == 0:
+        print()
+        time.sleep(2)
+        print("Hours Left [", hoursLeft, "]")
+        print()
+        time.sleep(1)
+        print("*You ran out of time*")
+        playAgain = ""
+        while playAgain != "yes" and playAgain != "y":
+            playAgain = input("Do you want to play again? [Yes] or [Y] to continue playing: ").lower()
+        if playAgain == "yes" or "y":
+            print()
+            print("*Starting from checkpoint*")
+            rover1()
+        else:
+            return
+        return
+
+
 #intro()
 
 #name = input("What is your name?: ").lower()
@@ -295,5 +573,7 @@ name = "Daniel"
 
 #intro2()
 #part1()
-rover1()
+#rover1()
+#rover2()
+rover3()
 
